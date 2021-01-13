34bd70c5423dc8e18bc3e67349ea87ee232cf70f
ranggatw <rangga.public@gmail.com>
2021-01-13 10:45:40 +0700

    fungsi aggregat

--- 11-Group-Function/Group-Function.sql
+++ 11-Group-Function/Group-Function.sql
@@ -1,5 +1,5 @@
 -- Function Agregatation --
--- => 
+-- => Fungsi Pengumpulan 
 
 SELECT 
     round(avg(salary), 2) as rata_rata_gaji,
@@ -38,5 +38,27 @@ FROM
 WHERE 
     employee_id in (132,128, 136, 127, 135, 140, 144, 119,131, 182);
 
+-- AGGREGATION  with format value function--
+SELECT
+    to_char(sum(salary), 'FM999,999') as total_salary,
+    to_char(min(salary) , 'FM999,999') as min_salary,
+    to_char(max(salary), 'FM999,999') as max_salary,
+    to_char(count(salary), 'FM999,999') as count_rows,
+    to_char(avg(salary), 'FM999,999') as average_salary
+FROM 
+    employees
+WHERE 
+    employee_id in (132,128, 136, 127, 135, 140, 144, 119,131, 182);
 
--- Output : menampilkan employee_id, dan salary 
+-- Penggunaan GROUP aggregat harus semuanya merupakan fungsi aggregat kalau ada yang tidak maka akan muncul error
+SELECT
+    employee_id,
+    to_char(sum(salary), 'FM999,999') as total_salary,
+    to_char(min(salary) , 'FM999,999') as min_salary,
+    to_char(max(salary), 'FM999,999') as max_salary,
+    to_char(count(salary), 'FM999,999') as count_rows,
+    to_char(avg(salary), 'FM999,999') as average_salary
+FROM 
+    employees
+WHERE 
+    employee_id in (132,128, 136, 127, 135, 140, 144, 119,131, 182);
